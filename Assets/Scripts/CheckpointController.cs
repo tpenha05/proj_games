09e09645ba60ba06390624efcdea1377e6c24ee5
Henrique Turco <henriqueturco1@gmail.com>
2025-05-14 10:07:59 -0300

    Checkpoints prontos

--- Assets/Scripts/CheckpointController.cs
+++ Assets/Scripts/CheckpointController.cs
@@ -1,25 +1,44 @@
-// CheckpointController.cs
+using System.Collections;
 using UnityEngine;
 using TMPro;
 
 public class CheckpointController : MonoBehaviour
 {
-    [Header("Referências")]
-    // public Animator lightAnimator;
-    public GameObject uiCanvas;        // Canvas/TextInteract
-    public bool isSpawn = false;
-    public Transform arrow;            // só para spawn
+    [Header("Sprites e timings")]
+    public Sprite offSprite;
+    public Sprite[] activateSprites;
+    public Sprite[] onSprites;
+    public float frameTime = 0.1f;
 
-    private bool playerNearby = false;
+    [Header("Referências")]
+    public bool isSpawn = false;
+    public Transform arrow;
+    public GameObject uiCanvas;
+    private TextMeshProUGUI uiText;
+    private SpriteRenderer sr;
+    private Coroutine idleLoopCoroutine;
+    private bool playerNearby;
     private Transform player;
+
+    [Header("Debug")]
+    [ReadOnly] public bool isActiveCheckpoint;
+
+    private string lastUIText = "";
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        uiText = uiCanvas.GetComponentInChildren<TextMeshProUGUI>();
+        uiCanvas.SetActive(false);
+
+        if (isSpawn && arrow != null)
+            arrow.gameObject.SetActive(false);
+    }
 
     void Start()
     {
-        // estado inicial
-        // lightAnimator.Play(isSpawn ? "Shining" : "Weak");
-        uiCanvas.SetActive(false);
-        if (isSpawn)
-            arrow.gameObject.SetActive(true);
+        if (isSpawn) CheckpointManager.I.RegisterSpawn(this);
+        else sr.sprite = offSprite;
     }
 
     void Update()
@@ -30,55 +49,117 @@ public class CheckpointController : MonoBehaviour
         {
             if (isSpawn)
             {
-                // Teleport do player para o checkpoint ativo
-                var cp = CheckpointManager.I.activeCheckpoint;
-                if (cp != null && cp != this)
-                    player.position = cp.transform.position;
+                var active = CheckpointManager.I.activeCheckpoint;
+                if (active != null)
+                {
+                    player.position = active.transform.position;
+                }
             }
             else
             {
-                // Ativa este checkpoint
-                CheckpointManager.I.SetActive(this);
+                if (CheckpointManager.I.activeCheckpoint == this)
+                    player.position = CheckpointManager.I.spawnCheckpoint.transform.position;
+                else
+                    CheckpointManager.I.SetActive(this);
             }
         }
 
-        // se for spawn, faz a seta apontar
-        if (isSpawn && CheckpointManager.I.activeCheckpoint != null)
+        // Atualiza texto da UI dinamicamente
+        if (uiCanvas.activeSelf)
         {
-            Vector3 dir = (CheckpointManager.I.activeCheckpoint.transform.position 
-                           - arrow.position).normalized;
-            float ang = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            arrow.rotation = Quaternion.Euler(0, 0, ang - 90f);
+            string desiredText = "";
+
+            if (isSpawn)
+            {
+                desiredText = (CheckpointManager.I.activeCheckpoint != null)
+                    ? "Teleport [M]"
+                    : "Needs an activated totem";
+            }
+            else
+            {
+                desiredText = (CheckpointManager.I.activeCheckpoint == this)
+                    ? "Teleport [M]"
+                    : "Activate [M]";
+            }
+
+            if (desiredText != lastUIText)
+            {
+                uiText.text = desiredText;
+                lastUIText = desiredText;
+            }
+        }
+
+        // Atualiza seta do spawn
+        if (isSpawn && arrow != null)
+        {
+            var active = CheckpointManager.I.activeCheckpoint;
+            if (active != null)
+            {
+                arrow.gameObject.SetActive(true);
+                Vector3 dir = (active.transform.position - arrow.position).normalized;
+                float ang = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                arrow.rotation = Quaternion.Euler(0, 0, ang - 90f);
+            }
+            else
+            {
+                arrow.gameObject.SetActive(false);
+            }
         }
     }
 
-    void OnTriggerEnter2D(Collider2D c)
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if (c.CompareTag("Player"))
+        if (!other.CompareTag("Player")) return;
+
+        playerNearby = true;
+        player = other.transform;
+        uiCanvas.SetActive(true);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        playerNearby = false;
+        uiCanvas.SetActive(false);
+    }
+
+    public void ForceOn()
+    {
+        StopAllCoroutines();
+        sr.sprite = onSprites.Length > 0 ? onSprites[0] : offSprite;
+        idleLoopCoroutine = StartCoroutine(LoopSprites(onSprites, true));
+    }
+
+    public void ForceOff()
+    {
+        StopAllCoroutines();
+        sr.sprite = offSprite;
+    }
+
+    public void PlayActivateAnimation()
+    {
+        StopAllCoroutines();
+        StartCoroutine(ActivateRoutine());
+    }
+
+    private IEnumerator ActivateRoutine()
+    {
+        yield return LoopSprites(activateSprites, false);
+        idleLoopCoroutine = StartCoroutine(LoopSprites(onSprites, true));
+    }
+
+    private IEnumerator LoopSprites(Sprite[] sprites, bool loop)
+    {
+        if (sprites == null || sprites.Length == 0) yield break;
+
+        do
         {
-            playerNearby = true;
-            player = c.transform;
-            uiCanvas.SetActive(true);
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D c)
-    {
-        if (c.CompareTag("Player"))
-        {
-            playerNearby = false;
-            uiCanvas.SetActive(false);
-        }
-    }
-
-    // Chamado pelo Manager
-    public void Activate()
-    {
-        // lightAnimator.Play("Shining");
-    }
-
-    public void Deactivate()
-    {
-        // lightAnimator.Play("Weak");
+            foreach (var s in sprites)
+            {
+                sr.sprite = s;
+                yield return new WaitForSeconds(frameTime);
+            }
+        } while (loop);
     }
 }
--- Assets/Scripts/CheckpointManager.cs
+++ Assets/Scripts/CheckpointManager.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 public class CheckpointManager : MonoBehaviour
 {
     public static CheckpointManager I;
-    public CheckpointController activeCheckpoint;
+    [HideInInspector] public CheckpointController activeCheckpoint;
+    [HideInInspector] public CheckpointController spawnCheckpoint;
 
     void Awake()
     {
@@ -12,11 +13,26 @@ public class CheckpointManager : MonoBehaviour
         else Destroy(gameObject);
     }
 
+    // Chamado pelo spawn no Start()
+    public void RegisterSpawn(CheckpointController cp)
+    {
+        spawnCheckpoint = cp;
+        // Deixe o spawn “aceso” por padrão
+        cp.ForceOn();
+    }
+
+    // Ativa um novo checkpoint
     public void SetActive(CheckpointController cp)
     {
         if (activeCheckpoint != null && activeCheckpoint != cp)
-            activeCheckpoint.Deactivate();
+        {
+            activeCheckpoint.ForceOff();
+            activeCheckpoint.isActiveCheckpoint = false;
+        }
+
         activeCheckpoint = cp;
-        cp.Activate(); 
+        cp.isActiveCheckpoint = true;
+        cp.PlayActivateAnimation();
     }
+
 }
