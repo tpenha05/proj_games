b7b1b9f60e2d6bf74ed2e68aae10d1f2139149a6
Henrique Turco <henriqueturco1@gmail.com>
2025-05-14 15:39:11 -0300

    parallax pronto

--- Assets/Scripts/ParallaxLayer.cs
+++ Assets/Scripts/ParallaxLayer.cs
@@ -1,21 +1,46 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Transform))]
 public class ParallaxLayer : MonoBehaviour
 {
-    public float parallaxMultiplier = 0.5f;
+    [Range(0f,1f)]
+    public float parallaxFactor = 0.5f;
+
+    public float stopParallaxY = 5f;
+    public Transform player;
+
     private Transform cam;
-    private Vector3 previousCamPos;
+    private Vector3 layerStartPos;
+    private Vector3 camStartPos;
+    private float lastAllowedCamX;
 
     void Start()
     {
         cam = Camera.main.transform;
-        previousCamPos = cam.position;
+        layerStartPos = transform.position;
+        camStartPos   = cam.position;
+        lastAllowedCamX = 0f;
+
+        if (player == null)
+            player = GameObject.FindWithTag("Player")?.transform;
     }
 
     void LateUpdate()
     {
-        Vector3 deltaMovement = cam.position - previousCamPos;
-        transform.position += new Vector3(deltaMovement.x * parallaxMultiplier, 0, 0);
-        previousCamPos = cam.position;
+        // deslocamento total da câmera desde o início, só X
+        float totalCamDeltaX = cam.position.x - camStartPos.x;
+
+        // se ultrapassar a altura, congela no último X permitido
+        if (player != null && player.position.y > stopParallaxY)
+            totalCamDeltaX = lastAllowedCamX;
+        else
+            lastAllowedCamX = totalCamDeltaX;
+
+        // posiciona só no X, mantém Y/Z originais
+        transform.position = new Vector3(
+            layerStartPos.x + totalCamDeltaX * parallaxFactor,
+            layerStartPos.y,
+            layerStartPos.z
+        );
     }
 }
