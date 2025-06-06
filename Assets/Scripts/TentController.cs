6f650ab70c17f2bd93c116d629ef07d42a326bb7
Henrique Mayor <henriquessm.al.insper.edu.br>
2025-06-06 13:51:48 -0300

    tenda da pra interagir

--- Assets/Scripts/TentController.cs
+++ Assets/Scripts/TentController.cs
@@ -6,42 +6,61 @@ public class TentController : MonoBehaviour
     public GameObject healingFXPrefab; // Prefab de brilho
 
     public GameObject adPanel;
+    private bool hasInteracted = false;
+
+
+    public GameObject interactCanvas; // Botão de interação
     private bool isPlayerNearby = false;
     private PlayerHealth playerHealth;
 
     void Start()
     {
         pressETextUI.SetActive(false);
+        interactCanvas.SetActive(false);
     }
 
     void Update()
     {
-        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
+
+        if (!hasInteracted && isPlayerNearby)
         {
-            adPanel.SetActive(true);
-            if (playerHealth != null)
+            interactCanvas.SetActive(true);
+        }
+        else
+        {
+            interactCanvas.SetActive(false);
+        }
+    }
+
+
+    public void OnInteractButtonPressed()
+    {
+        
+        adPanel.SetActive(true);
+        interactCanvas.SetActive(false);
+        hasInteracted = true;
+        if (playerHealth != null)
+        {
+            if (playerHealth.getMaxHealth() == playerHealth.GetCurrentHealth())
             {
-                if (playerHealth.getMaxHealth() == playerHealth.GetCurrentHealth())
+                playerHealth.increaseMaxHealth(1);
+            }
+            else
+            {
+                int currentHealth = playerHealth.GetCurrentHealth();
+                int max = playerHealth.getMaxHealth();
+                for (int i = currentHealth; i < max; i++)
                 {
-                    playerHealth.increaseMaxHealth(1);
-                }
-                else
-                {
-                    int currentHealth = playerHealth.GetCurrentHealth();
-                    int max = playerHealth.getMaxHealth();
-                    for (int i = currentHealth; i < max; i++)
-                    {
-                        playerHealth.Heal();
+                    playerHealth.Heal();
                     if (healingFXPrefab != null)
                     {
                         GameObject fx = Instantiate(healingFXPrefab, playerHealth.transform.position, Quaternion.identity);
                         Destroy(fx, 1.5f); // ajuste conforme a duração real do FX
                     }
-                    }
                 }
-
-                pressETextUI.SetActive(false);
             }
+
+            pressETextUI.SetActive(false);
         }
     }
 
